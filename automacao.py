98cd394b9d19f22bef463a0f0f65c4c335dbe8bc
Renaolol <renanschmeier91@gmail.com>
2025-12-22 17:34:14 -0300

    Update

--- automacao.py
+++ automacao.py
@@ -1,16 +1,7 @@
 import pyautogui
 import pymsgbox
 from pathlib import Path
-# print('Press Ctrl-C to quit.')
-# try:
-#     while True:
-#         x, y = pyautogui.position()
-#         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-#         print(positionStr, end='')
-#         print('\b' * len(positionStr), end='', flush=True)
-# except KeyboardInterrupt:
-#     print('\n')
-# pyautogui.sleep(2)
+pyautogui.FailSafeException
 def abre_dominio():
     pasta_atual = Path(__file__).parent
     pasta_imagens = pasta_atual / 'imagens'
@@ -19,33 +10,43 @@ def abre_dominio():
     senha_dominio = pasta_imagens / 'senha_dominio.png'
     ok_dominio = pasta_imagens / 'ok.png'
     if pasta_dominio.is_file():
-        pasta_dominio_mover = pyautogui.locateOnScreen(str(pasta_dominio))
+        pasta_dominio_mover = pyautogui.locateOnScreen(str(pasta_dominio),confidence=0.9)
         pyautogui.moveTo(pasta_dominio_mover,duration=1)
         pyautogui.doubleClick()
         pyautogui.sleep(1)
         if dominio_icone.is_file():
-            icone_dominio_mover = pyautogui.locateOnScreen(str(dominio_icone))
+            icone_dominio_mover = pyautogui.locateOnScreen(str(dominio_icone),confidence=0.9)
             pyautogui.moveTo(icone_dominio_mover,duration=1)
             pyautogui.doubleClick()
             pyautogui.sleep(2)
             while True:
                 print("Aguardando tela de login...")
-                pyautogui.sleep(2)
                 try:
-                    pyautogui.locateOnScreen(str(senha_dominio))
+                    pyautogui.locateOnScreen(str(senha_dominio),confidence=0.9)
                     break
                 except:
                     print("Reprocessando...")
                     pass
-            senha_mover = pyautogui.locateOnScreen(str(senha_dominio))
+            senha_mover = pyautogui.locateOnScreen(str(senha_dominio),confidence=0.9)
             pyautogui.moveTo(senha_mover,duration=1)
             pyautogui.click()
             pyautogui.write("RS",interval=0.01)
-            ok_mover = pyautogui.locateOnScreen(str(ok_dominio))
+            ok_mover = pyautogui.locateOnScreen(str(ok_dominio),confidence=0.9)
             pyautogui.moveTo(ok_mover,duration=1)
             pyautogui.click()
     else:
         print('Erro, pasta não encontrada')
     return
-pymsgbox.alert(text="teste",title="teste de título")
-abre_dominio()
+
+
+pasta_atual = Path(__file__).parent
+pasta_imagens = pasta_atual / 'imagens'
+dominio_barra = pasta_imagens /'dominio_barra_tarefas.png'
+try:
+    dominio_barra_identificacao = pyautogui.locateOnScreen(str(dominio_barra),confidence=0.9)
+except:
+    dominio_barra_identificacao=False
+if dominio_barra_identificacao:
+    print("Domínio já está aberta...")
+else:
+    abre_dominio()
