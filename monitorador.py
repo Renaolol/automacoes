539ee95493782267d3f3ca4b123cce3b51832b60
Renaolol <renanschmeier91@gmail.com>
2026-01-23 11:50:25 -0300

    monitorador.py updated
    App faz a busca no computador e retorna os componentes, e já grava em um banco de dados

--- monitorador.py
+++ monitorador.py
@@ -3,45 +3,81 @@ import platform
 import cpuinfo
 import wmi
 import dearpygui.dearpygui as dpg
-
+import pyodbc
+import psycopg2
+import pythoncom
+c = wmi.WMI()
+CONEXAO={
+    "host": "10.0.0.10",
+    "dbname": "inventario_computadores",
+    "user": "postgres",
+    "password": "0176",
+    "port":"5432",
+}
 
 def salvar(sender, app_data,user_data):
-    print(user_data)
+    conn = psycopg2.connect(**CONEXAO)
+    cursor = conn.cursor()
+    query ="""
+            INSERT INTO computadores (pc_name, marca_mb, modelo_mb, serie_mb,placa_video,
+                                    processador, nucleos, memoria_ram_gb, sistema_operacional, 
+                                    armazenamento)
+            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+            """
+    
+    cursor.execute(query, (user_data[0],user_data[1],user_data[2],user_data[3],
+                          user_data[4],user_data[5],user_data[6],user_data[7],
+                          user_data[8],str(user_data[9])))
+    conn.commit()
+    conn.close()
+
+    print(user_data[0:9])
+    for x in user_data[9]:
+        print(x)
+
+def verificar_conexao():
+    conn = psycopg2.connect(**CONEXAO)
+    if conn:
+        return True
+    else:   
+        return False
+conexao_feita=verificar_conexao()
+
+
 
 dpg.create_context()
 
-c = wmi.WMI()
-for board in c.Win32_BaseBoard():
-    marca_mb = board.Manufacturer
-    modelo_mb = board.Product
-    serie_mb = board.SerialNumber
+def search_componentes():
+    pythoncom.CoInitialize()
+    c = wmi.WMI()
+    for board in c.Win32_BaseBoard():
+        marca_mb = board.Manufacturer
+        modelo_mb = board.Product
+        serie_mb = board.SerialNumber
 
-for video in c.Win32_VideoController():
-    video = video.Description
-#infos do CPU
-#Nome do Computador
-pc_name = platform.node()
-#modelo do Processador ["brand_raw"]
-processor = cpuinfo.get_cpu_info()
-modelo_processador = processor["brand_raw"]
+    for video in c.Win32_VideoController():
+        video = video.Description
+    #infos do CPU
+    #Nome do Computador
+    pc_name = platform.node()
+    #modelo do Processador ["brand_raw"]
+    processor = cpuinfo.get_cpu_info()
+    modelo_processador = processor["brand_raw"]
 
-#nucleos de processamento
-cpu_count = psutil.cpu_count()
+    #nucleos de processamento
+    cpu_count = psutil.cpu_count()
 
-#infos da Memória
-mem = psutil.virtual_memory()
-mem_total = round(mem.total/1073741824,2)
+    #infos da Memória
+    mem = psutil.virtual_memory()
+    mem_total = round(mem.total/1073741824,2)
 
-#Versão do Windows
-for os in c.Win32_OperatingSystem():
-    sys_op = os.Caption
-    
-#infos HD/SSD
-disk = psutil.disk_partitions()
-
-
-with dpg.window(tag="Componentes"):
-    with dpg.group():
+    #Versão do Windows
+    for os in c.Win32_OperatingSystem():
+        sys_op = os.Caption
+        
+    #infos HD/SSD
+    disk = psutil.disk_partitions()
+    with dpg.group(parent="Componentes"):
         dpg.add_text(f"Nome:                -> {pc_name}")
         dpg.add_text(f"Marca Placa Mãe:     -> {marca_mb}")
         dpg.add_text(f"Modelo Placa Mãe:    -> {modelo_mb}")
@@ -64,11 +100,16 @@ with dpg.window(tag="Componentes"):
                     dpg.add_text(f' Free {round(free/1073741824,2)}Gb')
                 except Exception as e:
                     pass
+        dpg.add_button(label="Salvar",callback=salvar,user_data=[pc_name,marca_mb,modelo_mb,serie_mb,video,
+                                                                modelo_processador,cpu_count,mem_total,
+                                                                sys_op,lista_discos])
+
+with dpg.window(tag="Componentes"):
     with dpg.group(horizontal=True):
         dpg.add_button(label="Fechar",callback=dpg.destroy_context)
-        dpg.add_button(label="Salvar",callback=salvar,user_data=[pc_name,marca_mb,modelo_mb,serie_mb,video,
-                                                                 modelo_processador,cpu_count,mem_total,
-                                                                 sys_op,lista_discos])
+        dpg.add_button(label="Buscar componentes",callback=search_componentes)
+        dpg.add_text(("Conexão bem sucedida") if conexao_feita == True else ("Aguardando conexão"))
+
 dpg.create_viewport(title="Inventario de PCs",width=600,height=400)
 dpg.setup_dearpygui()
 dpg.show_viewport()
--- teste.py
+++
@@ -1,4 +0,0 @@
-from contabil import *
-
-lancto = LanctoContabil(100,55,5,"Compra de Estoque")
-print(help(lancto))
